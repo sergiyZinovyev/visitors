0da3ac10693b37e230012ec935d522e2baf711be
sergiyZinovyev <thrion1@gmail.com>
2020-03-19 13:50:04 +0200

    0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
+import { LoginComponent } from './login/login.component';
+import { VisisitorComponent } from './visisitor/visisitor.component';
+import { InviteComponent } from './invite/invite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VisisitorComponent,
+    InviteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/visisitor/visisitor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visisitor',
+  templateUrl: './visisitor.component.html',
+  styleUrls: ['./visisitor.component.css']
+})
+export class VisisitorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
